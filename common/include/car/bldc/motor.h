5af2e0a353bb2d48731b8438efc7d1685626ecfc
Markus Bader <markus.bader@mx-robotics.com>
2021-01-31 17:04:10 +0100

    MotorClassupdate

--- common/include/car/bldc/motor.h
+++ common/include/car/bldc/motor.h
@@ -5,6 +5,7 @@
 #include <array>
 #include <car/math/measurement.h>
 #include <car/math/angle.h>
+#include <functional>
 
 namespace car {
 namespace bldc {
@@ -12,11 +13,12 @@ namespace bldc {
 
 class Motor {
     public:
-        Motor(std::array<uint8_t, 3> _pins_INH, std::array<uint8_t, 3> _pins_PWN, std::array<uint8_t, 3> _pins_IS, uint8_t _pin_CS) 
+        Motor(std::array<uint8_t, 3> _pins_INH, std::array<uint8_t, 3> _pins_PWN, std::array<uint8_t, 3> _pins_IS, uint8_t _pin_CS, car::math::Direction encoder_direction) 
         : pins_INH(_pins_INH) 
         , pins_PWN(_pins_PWN)
         , pins_IS(_pins_IS)
         , pin_CS(_pin_CS) 
+        , encoder_direction(encoder_direction) 
         , coupled(false){
             sprintf(info, "INH: <%d, %d, %d>,  PWM: <%d, %d, %d>,  IS: <%d, %d, %d>, CS: %d",
             pins_INH[0], pins_INH[1], pins_INH[2], 
@@ -27,23 +29,40 @@ class Motor {
         std::array<uint8_t, 3> pins_PWN;
         std::array<uint8_t, 3> pins_IS;
         uint8_t pin_CS;
+        car::math::Direction encoder_direction;
         bool coupled;  /// on true H-Bridges are enabled 
 
         char info[200];
 
         float target_PWN[3];
         car::math::Measurement<car::math::Angle14Bit> position_delta;
-        car::math::Measurement<car::math::Angle14Bit> position_current;
+        car::math::Measurement<car::math::Angle14Bit> position;
+        car::math::AngleDeg phase_angle;
+        car::math::AngleDeg pwm_angle;
         float rpm;
-        float flux_offest;
-        float flux_angle;
         
         volatile uint32_t* timer_register[3];
-        void update(const car::math::Measurement<car::math::Angle14Bit> &motor_position){
-            position_delta = motor_position - position_current;
-            position_current = motor_position;
+        /**
+         * function to update wheel state
+         **/
+        void update_position(){
+            car::math::Measurement<car::math::Angle14Bit> measurement; // encoder measurement
+            read(pin_CS, measurement.value(), measurement.stamp);      // read angle encoder 
+
+            /// fix encoder mount angle
+            if(encoder_direction == car::math::Direction::COUNTERCLOCKWISE) measurement.value() = measurement.value.max() - measurement.value();
+            int16_t phase_measurement_max = measurement.value.max() / 11;
+            int16_t phase_measurement = measurement.value() % phase_measurement_max;
+            phase_angle.set(phase_measurement * phase_angle.max() / phase_measurement_max);
+            position_delta = measurement - position;
+            position = measurement;
             rpm = position_delta.value.get_norm() / position_delta.stamp_as_sec();
         }
+
+        /**
+         * function pointer to read moder angle encoder
+         **/
+        std::function<void (uint8_t cs, int16_t &value, uint32_t &stamp)> read;
 };
 
 }
--- examples/main_bldc.cpp
+++ examples/main_bldc.cpp
@@ -19,10 +19,10 @@ car::encoder::Encoder *encoder;
 car::bldc::Driver driver;
 car::bldc::Motor motor0(std::array<uint8_t, 3>({33, 26, 31}),
                         std::array<uint8_t, 3>({10, 22, 23}),
-                        std::array<uint8_t, 3>({A15, A16, A17}), 2);
+                        std::array<uint8_t, 3>({A15, A16, A17}), 2, car::math::Direction::COUNTERCLOCKWISE);
 car::bldc::Motor motor1(std::array<uint8_t, 3>({28, 8, 25}),
                         std::array<uint8_t, 3>({5, 6, 9}),
-                        std::array<uint8_t, 3>({A15, A16, A17}), 14);
+                        std::array<uint8_t, 3>({A15, A16, A17}), 14, car::math::Direction::COUNTERCLOCKWISE);
 int loop_count = 0;
 
 char txt[200];
@@ -32,6 +32,7 @@ struct Measurment
   car::math::AngleDeg motor_phase_angle;
   car::math::AngleDeg svm_phase_angle;
 };
+int32_t resolution_phase;
 uint32_t timer_count;
 Measurment m;
 static const size_t buffer_size_max = 1000;
@@ -43,7 +44,7 @@ void setup()
   car::math::AngleDeg::init();
   m.svm_phase_angle = 0;
   driver.init(&motor0, &motor1);
-  encoder = new car::encoder::AS5048A(SPI_SCK, std::array<uint8_t, 2>({motor0.pin_CS, motor1.pin_CS}));
+  encoder = new car::encoder::AS5048A(std::array<uint8_t, 2>({motor0.pin_CS, motor1.pin_CS}), SPI_SCK);
 
   Serial.begin(115200); /// init serial
   while (!Serial) ;
@@ -61,15 +62,17 @@ void setup()
   *motor1.timer_register[0] = 0;
   *motor1.timer_register[1] = 0;
   *motor1.timer_register[2] = 0;
+  
+  motor0.read = std::bind(&car::encoder::Encoder::read, encoder, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+  motor1.read = std::bind(&car::encoder::Encoder::read, encoder, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+  resolution_phase = encoder->resolution() / 11;
 }
 // the loop routine runs over and over again forever:
 void loop()
 {
-  int32_t resolution_phase = encoder->resolution() / 11;
-  m.encoder_motor.value = encoder->get_raw(car::encoder::cs0, &m.encoder_motor.stamp);
-  int32_t encoder_phase = resolution_phase - m.encoder_motor.value() % resolution_phase;
+  motor0.update_position();
   //car::math::Angle encoder_motor_angle = ((m.encoder_motor.value * 360l) / encoder->resolution());
-  m.motor_phase_angle = ((encoder_phase * 360l) / resolution_phase);
+  m.motor_phase_angle = motor0.phase_angle;
   m.svm_phase_angle = m.motor_phase_angle - 65;
   //m.svm_phase_angle.normalize();
 
@@ -87,11 +90,10 @@ void loop()
 
   if (true && (loop_count % 100 == 0))
   {
-    motor0.update(m.encoder_motor);
     measurments[buffer_size++] = m;
     if(true){
       if(loop_count == 0)  Serial.println("# m.encoder_motor.stamp, m.encoder_motor.value, m.motor_phase_angle, m.svm_phase_angle()");
-      sprintf(txt, "%8lu, %5u, %5d, %5d, %8lu, %5u, %5.4f", m.encoder_motor.stamp, m.encoder_motor.value(), m.motor_phase_angle(), m.svm_phase_angle(), motor0.position_delta.stamp, motor0.position_delta.value(), motor0.rpm);
+      sprintf(txt, "%8lu, %5u, %5d, %5d, %8lu, %5u, %5.4f", m.encoder_motor.stamp, motor0.position.value(), m.motor_phase_angle(), m.svm_phase_angle(), motor0.position_delta.stamp, motor0.position_delta.value(), motor0.rpm);
       Serial.println(txt);
     }
   }
@@ -108,7 +110,7 @@ void loop()
       {
         m = measurments[i];
         if(loop_count == 0)  Serial.println("# m.encoder_motor.stamp, m.encoder_motor.value, m.motor_phase_angle, m.svm_phase_angle()");
-        sprintf(txt, "%8lu, %5u, %5d, %5d", m.encoder_motor.stamp, m.encoder_motor.value(), m.motor_phase_angle(), m.svm_phase_angle());
+        sprintf(txt, "%8lu, %5u, %5d, %5d", m.encoder_motor.stamp, motor0.position.value(), m.motor_phase_angle(), m.svm_phase_angle());
       
         Serial.println(txt);
         delay(1);
